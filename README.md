762b88a3ea32ef156580e69b8e75e931838c6e8a
Zach Sheley <zsheley@iu.edu>
2020-02-24 19:26:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock Paper Scissors
+
+This exercise asks students to play Rock, Paper, Scissors vs. a randomly-generated response from the computer.
---
+++ code/rps.py
@@ -0,0 +1,25 @@
+# Zach Sheley and Donald Long
+
+import random;
+
+# rock-0 paper-1 scissors-2
+
+def computer_choice():
+  computerChoice = random.randint(0,2)
+  print(f"Computer Choice: {computerChoice}")
+  return computerChoice
+
+def compare(user, computer):
+  if user==computer:
+    return("Tie")
+  elif user==0 and computer==2 or user==1 and computer==0 or user==2 and computer==1:
+    return("User")
+  else:
+    return("Computer")
+
+def main():
+  userChoice = int(input("Enter 0-Rock 1-Paper 2-Scissors "))
+  print(f"Winner: {compare(userChoice, computer_choice())}")
+  
+if __name__== "__main__":
+  main()
